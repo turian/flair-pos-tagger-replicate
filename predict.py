2042fd132d54dbdf3ede4d88ffaa87d6a05a5072
Joseph Turian <turian@gmail.com>
2023-07-12 22:47:49 +0200

    Added compression

--- predict.py
+++ predict.py
@@ -1,13 +1,17 @@
+import base64
+import json
 import pickle
+from typing import Any
+
+import gzlib
+
+# import lz4
+import snappy
 import torch
 from cog import BasePredictor, Input
 from flair.data import Sentence
 from flair.models import SequenceTagger
-from typing import Any
-import base64
-#import lz4
-import snappy
-import gzlib
+
 # TODO: Use safe pickle?
 
 
@@ -15,7 +19,7 @@ class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         # TODO: Better to keep the model in the docker
-        #self.tagger = SequenceTagger.load("flair/pos-english")
+        # self.tagger = SequenceTagger.load("flair/pos-english")
         self.tagger = SequenceTagger.load("pos-english/pytorch_model.bin")
 
     @staticmethod
@@ -23,9 +27,15 @@ class Predictor(BasePredictor):
         assert isinstance(text, str)
         return Sentence(text)
 
-    def predict(self,
-            sentences_json: str = Input(description="JSON of sentence strings (or individual sentence string) to POS tag")
-            compression: str = Input(description="Compression to use: snappy (default) / none / lz4 (unimplemented) / gzip", default="snappy")
+    def predict(
+        self,
+        sentences_json: str = Input(
+            description="JSON of sentence strings (or individual sentence string) to POS tag"
+        ),
+        compression: str = Input(
+            description="Compression to use: snappy (default) / none / lz4 (unimplemented) / gzip",
+            default="snappy",
+        ),
     ) -> str:
         with torch.no_grad():
             sentences = json.loads(sentence)
@@ -39,11 +49,11 @@ class Predictor(BasePredictor):
                 sentence = Predictor.str_to_sentence(sentence)
                 results = sentence
             pkl = pickle.dumps(results)
-            if compression == "none"
+            if compression == "none":
                 pkl = pkl
-            elif compression = "snappy":
+            elif compression == "snappy":
                 pkl = snappy.compress(pkl)
-            elif commpression = "gzip":
+            elif commpression == "gzip":
                 pkl = gzip.compress(pkl)
             else:
                 assert False, f"Unknown compression: {compression}"
